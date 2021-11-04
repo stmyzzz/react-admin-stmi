faf455abfa0efad2fef8456c6c723bf009599215
dlyan.ding <dlyan.ding@huolala.cn>
2021-11-04 10:58:47 +0800

    init:代码优化

--- src/router/outerRouter/OuterRouter.tsx
+++ src/router/outerRouter/OuterRouter.tsx
@@ -4,8 +4,8 @@
  * @Author: dlyan.ding
  * @Date: 2021-09-28 10:59:53
  * @LastEditors: dlyan.ding
- * @LastEditTime: 2021-10-15 09:29:39
+ * @LastEditTime: 2021-11-04 10:57:36
  */
-import React, { Suspense } from "react";
-import { Switch, Router, RouteProps } from "react-router-dom";
-import PageLoading from "@/components/base/page-loading";
+import React, { Suspense } from 'react'
+import { Switch, Router, RouteProps } from 'react-router-dom'
+import PageLoading from '@/components/base/page-loading'
