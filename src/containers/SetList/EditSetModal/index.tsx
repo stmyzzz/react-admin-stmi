9c6aea34322e7fbd69d01cfde14f6c128ace0e2c
dlyan.ding <dlyan.ding@huolala.cn>
2021-11-05 21:10:42 +0800

    feat:创建配置列表

---
+++ src/containers/SetList/EditSetModal/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: dlyan.ding
+ * @Date: 2021-11-05 21:07:42
+ * @LastEditors: dlyan.ding
+ * @LastEditTime: 2021-11-05 21:08:24
+ */
+const EditSetModal = () => {
+  return <div>123</div>
+}
+
+export default EditSetModal
--- src/pages/SetList/index.tsx
+++ src/pages/SetList/index.tsx
@@ -4,7 +4,7 @@
  * @Author: stmy.ding
  * @Date: 2021-09-29 13:52:36
  * @LastEditors: dlyan.ding
- * @LastEditTime: 2021-11-05 20:42:45
+ * @LastEditTime: 2021-11-05 21:00:45
  */
 import MainLayout from '@/components/MainLayout'
 import { Card, Row, Col } from 'antd'
@@ -18,7 +18,7 @@ const SetList = () => {
           <Card title='911' extra={<EditFilled />} bordered={false}>
             <Image
               src='https://files.porsche.cn/filestore/image/multimedia/none/992-c2-modelimage-sideshot/model/5d3635ab-d97f-11eb-80d9-005056bbdc38;sN;twebp/porsche-model.webp'
-              width={300}
+              width={'100%'}
               preview={false}
             />
           </Card>
@@ -27,7 +27,7 @@ const SetList = () => {
           <Card title='718' bordered={false}>
             <Image
               src='https://files.porsche.cn/filestore/image/multimedia/none/982-718-c7-modelimage-sideshot/model/1b1b5cb6-e874-11ea-80cd-005056bbdc38;sN;twebp/porsche-model.webp'
-              width={300}
+              width={'100%'}
               preview={false}
             />
           </Card>
@@ -36,7 +36,7 @@ const SetList = () => {
           <Card title='panamera' bordered={false}>
             <Image
               src='https://files.porsche.cn/filestore/image/multimedia/none/carrange-flyout-718/thumbnail/f45e2ee9-2012-11ea-80c6-005056bbdc38;sS;twebp/porsche-thumbnail.webp'
-              width={300}
+              width={'100%'}
               preview={false}
             />
           </Card>
@@ -46,7 +46,7 @@ const SetList = () => {
           <Card title='panamera' bordered={false}>
             <Image
               src='https://files.porsche.cn/filestore/image/multimedia/none/carrange-flyout-718/thumbnail/f45e2ee9-2012-11ea-80c6-005056bbdc38;sS;twebp/porsche-thumbnail.webp'
-              width={300}
+              width={'100%'}
               preview={false}
             />
           </Card>
